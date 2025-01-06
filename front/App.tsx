18cacc4dcaef41f657ef097fbee39309f6ec94fc
load28 <tjalsdud89@naver.com>
2025-01-06 21:59:53 +0900

    refactor: move to front folder

---
+++ front/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <SafeAreaView>
+        <Text>Hello world!!</Text>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+}
+
+export default App;
